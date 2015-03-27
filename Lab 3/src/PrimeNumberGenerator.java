bc3ad5ef1e33a1adec1f39ee5eaed09ac5ca0804
JJGayso <gaysojj@rose-hulman.edu>
2015-03-26 23:29:21 -0400

    Code was beginning to become to long for individual cases, so a function was created to validate if a prime number or not, then those that were prime were added to the list.

--- Lab 3/src/PrimeNumberGenerator.java
+++ Lab 3/src/PrimeNumberGenerator.java
@@ -1,3 +1,4 @@
+import java.time.temporal.ValueRange;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,16 +7,23 @@ public class PrimeNumberGenerator {
 
 	public static List<Integer>generate(Integer inputNumber) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		if (inputNumber == 2) {
-			primes.add(1);
-		}
-		else if (inputNumber == 5) {
-			primes.add(1);
-			primes.add(2);
-			primes.add(3);
-			primes.add(5);
+		int i =1;
+		while (i < inputNumber) {
+			if (validate(i)) {
+				primes.add(i);
+			}
+			i++;
 		}
 		return primes;
 	}
+	
+	public static Boolean validate(final Integer primeNumber) {
+	      for (int i = 2; i < (primeNumber / 2); i++) {
+	         if (primeNumber % i == 0) {
+	            return false;
+	         }
+	      }
+	      return true;
+	   }
 	
 }
