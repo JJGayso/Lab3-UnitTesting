9c353ce5a05c471a466720bbbfa52c185f058df5
JJGayso <gaysojj@rose-hulman.edu>
2015-03-26 22:32:56 -0400

    Setting Up Test Class

--- Lab 3/src/PrimeNumberGeneratorTest.java
+++ Lab 3/src/PrimeNumberGeneratorTest.java
@@ -1,9 +1,20 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-
+@RunWith(Parameterized.class)
 public class PrimeNumberGeneratorTest {
+	private Integer inputNumber;
+	private Boolean expectedResult;
+	private PrimeNumberChecker primeNumberChecker;
+	
+	@Before
+	public void initialize() {
+		primeNumberChecker = new PrimeNumberChecker();
+		
+	}
 
 	@Test
 	public void test() {
